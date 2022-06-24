8dd1bbc32e6793aa1145892424a4bc2de81fbe7d
Robert <robertgz@users.noreply.github.com>
2022-06-24 00:41:45 -0700

    Test deploy 007 for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/ca-local-cftd'>
+    <BrowserRouter>
       <GQLApolloClient>
         <Routes>
 
