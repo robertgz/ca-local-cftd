28d387000cc4f5fe500f749c4abb96fc0aea5a85
Robert <robertgz@users.noreply.github.com>
2022-06-24 00:49:13 -0700

    Test deploy 008 for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { GQLApolloClient } from './GraphQL/GQLApolloClient';
+// import { GQLApolloClient } from './GraphQL/GQLApolloClient';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { Links } from './Links';
-import { CandidateCard } from './candidate/CandidateCard';
-import CandidatesAppBar from './candidates/CandidatesAppBar';
+// import { CandidateCard } from './candidate/CandidateCard';
+// import CandidatesAppBar from './candidates/CandidatesAppBar';
 
-const candidateId = 'd3d1c6e7-0add-49f1-9b3d-e9289e7efcf6|2022';
+// const candidateId = 'd3d1c6e7-0add-49f1-9b3d-e9289e7efcf6|2022';
 // const candidateId = 'be0a57fb-c0f0-bbd5-0d42-44a6560cbd21|2020';
 // const candidateId = '24738d25-2b55-4ef8-b78e-dcc4442a6327|2020';
 /**
@@ -25,8 +25,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    {/* <BrowserRouter basename='/ca-local-cftd'> */}
     <BrowserRouter>
-      <GQLApolloClient>
+      {/* <GQLApolloClient> */}
         <Routes>
 
           <Route 
@@ -36,12 +37,12 @@ root.render(
           </Route>
 
           <Route path="links" element={<Links/>}></Route>
-          <Route path="candidate" element={<CandidateCard candidateId={candidateId} />}></Route>
-          <Route path="candidates" element={<CandidatesAppBar />} />
+          {/* <Route path="candidate" element={<CandidateCard candidateId={candidateId} />}></Route>
+          <Route path="candidates" element={<CandidatesAppBar />} /> */}
 
           <Route path="app" element={<App/>}></Route>
         </Routes>
-      </GQLApolloClient>
+      {/* </GQLApolloClient> */}
     </BrowserRouter>
   </React.StrictMode>
 );
