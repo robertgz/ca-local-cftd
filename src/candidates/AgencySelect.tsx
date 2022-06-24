ff50fc19516ccd9a7a72fa155625a620e147c869
Robert <robertgz@users.noreply.github.com>
2022-06-23 17:25:46 -0700

    Added CandidatesAppBar and related components

---
+++ src/candidates/AgencySelect.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useQuery } from '@apollo/client';
+import { GET_AGENCIES } from '../GraphQL/GetAgenciesQuery';
+import { CandidateFilters } from '../candidate/models/CandidateFilters';
+
+interface Props {
+  filters: CandidateFilters;
+  setFilters: any;
+}
+
+export const AgencySelect = (props: Props) => {
+  const { filters, setFilters } = props;
+
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setFilters({
+      ...filters,
+      agencyIds: [event.target.value],
+    });
+  };
+
+  const GetAgencies = () => {
+    const { loading, error, data } = useQuery(GET_AGENCIES);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>`Error! ${error.message}`</p>;
+    const { agencies } = data;
+
+    const value = (filters.agencyIds?.[0]) ? (filters.agencyIds?.[0]) : '';
+
+    return (
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={value}
+        label="Agency"
+        onChange={handleChange}
+      >
+        {agencies?.map((agency: any) => (
+          <MenuItem key={agency.id} value={agency.id}>{agency.name}</MenuItem>
+        ))}
+      </Select>
+    )
+  }
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Agency</InputLabel>
+        <GetAgencies/>
+    </FormControl>
+  );
+}
