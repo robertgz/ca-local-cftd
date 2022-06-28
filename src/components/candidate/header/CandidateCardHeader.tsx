9c7ba6cf2638376980aabc2a8ebe603c14c41d39
Robert <robertgz@users.noreply.github.com>
2022-06-27 18:55:42 -0700

    Converted CandidateCardHeader to use CSS Modules

--- src/components/candidate/header/CandidateCardHeader.tsx
+++ src/components/candidate/header/CandidateCardHeader.tsx
@@ -5,14 +5,15 @@ import { CandidateCardImage } from './CandidateCardImage';
 import { CandidateCardName } from './CandidateCardName';
 import { CandidateCardDescription } from './CandidateCardDescription';
 import { CandidateCardWebsite } from './CandidateCardWebsite';
+import styles from './CandidateCardHeader.module.css';
 
 export const CandidateCardHeader: FunctionComponent = () => {
 
   return (
     <div>
-      <div style={styles.headerSection} >
+      <div className={styles.headerSection} >
         <CandidateCardImage/>
-        <div style={styles.textContainer}>
+        <div className={styles.textContainer}>
           <CandidateCardName/>
           <CandidateCardDescription/>  
           <CandidateCardWebsite/>  
@@ -21,26 +22,3 @@ export const CandidateCardHeader: FunctionComponent = () => {
     </div>
   );
 }
-
-
-export interface StylesDictionary{
-  [Key: string]: React.CSSProperties;
-}
-
-const DashBoardBlue = '#16375D';
-
-const styles: StylesDictionary = {
-  headerSection: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: '.5em 0',
-    borderBottom: `2px solid ${DashBoardBlue}`,
-  },
-  textContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    rowGap: '10px',
-    padding: '10px',
-  },
-};
